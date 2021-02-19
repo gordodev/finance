eacfd3f1fc57777e50db0c4043696b2f203ecd11
gordodev <carlylespeaks@gmail.com>
2021-02-19 01:12:32 -0500

    Build menu for OMS

---
+++ flintTrader/feeds/oms.py
@@ -0,0 +1,14 @@
+#Trading OMS that will track positions and P/L
+
+def enter_order():
+    print ('6 - Buy 100')
+    print ('7 - Buy 1000')
+    print ('8 - Buy 10000\n')
+    print ('3 - Sell 100')
+    print ('4 - Sell 100')
+    print ('5 - Buy 100')
+
+
+
+
+    option = input ('Select order')
