b28853953ca22452d89b2b07fb0dd3dbff1cb1e2
gordodev <carlylespeaks@gmail.com>
2021-02-20 01:26:48 -0500

    Position updates and full menu functions

--- flintTrader/feeds/oms.py
+++ flintTrader/feeds/oms.py
@@ -4,11 +4,21 @@ from os import system, name
 import time
 
 position = 0
-buying_power = 100000
+buying_power = 1000000
 side = 'NULL'
 cost = 0
 quote = 'NULL'
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # clear function 
 def clear(): 
@@ -22,7 +32,7 @@ def clear():
         _ = system('clear') 
 
 def enter_order():
-    global side, position, quantity
+    global side, position, quantity, quote
     print ('6 - Buy 100')
     print ('7 - Buy 1000')
     print ('8 - Buy 10000\n')
@@ -30,32 +40,58 @@ def enter_order():
     print ('4 - Sell 100')
     print ('5 - Buy 100')
 
-    print ('\nposition=',position)
-    print ('Buying power=',buying_power)
+    print ('\nPosition =',position)
+    print ('Buying power =',buying_power)
     print ('')
 
     option = input ('Select order: ')
     print ('')
     
+    #Set side & quantity
     if option == '6':
         side = 'buy'; quantity = 100
-        #print ('Run position update')
-        position_update()
+   
+    if option == '7':
+        side = 'buy'; quantity = 1000
 
+    if option == '8':
+        side = 'buy'; quantity = 10000
 
-    print (position)
-    print(get_quote())
+    if option == '3':
+        side = 'sell'; quantity = 100
+
+    if option == '4':
+        side = 'sell'; quantity = 1000
+
+    if option == '5':
+        side = 'sell'; quantity = 10000
+
+    quote=float(get_quote()) #Get price
+    position_update()        #Update position & buying power  
+
+    print ('NBBO: ',get_quote(),'\n')
+    #ENTER ORDER ^^
 
 def position_update():
     global position, buying_power
 
-    #print ('side: ',side)
     if side == 'buy':
-        print ('side is buy')
+        if (buying_power - float(quote * quantity)) < 0:
+            print (f"{bcolors.WARNING}ORDER REJECT: Insufficient funds{bcolors.ENDC}"); return
         position += quantity
-        cost = int(quote * quantity)#; buying_power -= cost 
-        print ('cost: ',cost,'buyingp:',buying_power)
-    time.sleep(2)
+        cost = float(quote * quantity); buying_power -= round((cost),2)
+        print ('Bought ',quantity)
+
+    elif side == 'sell':
+        if quantity > position:
+            print (f"{bcolors.WARNING}ORDER REJECT: Insufficient shares{bcolors.ENDC}")
+            print ('You tried to sell',quantity,'shares, but you only have',position,'!\n\n')
+            return
+        position -= quantity
+        cost = float(quote * quantity); buying_power += cost
+        print ('Sold ',quantity)
+
+    time.sleep(1)
     clear()
 
 
