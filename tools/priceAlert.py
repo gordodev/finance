ee015058df6f537afcc17dc91c38b57a2be2356a
Carl <carlylespeaks@gmail.com>
2021-03-04 02:08:20 -0500

    Update priceAlert.py
    Add price delta factor functionality and fix $0 price bug. Need to figure out double print bug.

--- tools/priceAlert.py
+++ tools/priceAlert.py
@@ -2,23 +2,39 @@ import time
 from yahoo_fin import stock_info as si
 from playsound import playsound
 import sys
+import os
 
 #Text to speech libraries
 import pyttsx3
 engine = pyttsx3.init()
 engine.setProperty('rate', 255)  #Speed
 
+
 #from Tkinter import *
 
 import os
-os.system('color 0f') # sets the background to black
+os.system('color 0f') # activate defaul color scheme 
 
 lastPx = "NULL"
-PxDelta = 0
+
+print ('Arguments: ',len(sys.argv))
+time.sleep(1)
+
+
+if len(sys.argv) > 5:
+    PxDelta = 0; delta1=0.3+float(sys.argv[5]); delta2=0.6+float(sys.argv[5]) #PxDelta levels 1/2
+else:
+    PxDelta = 1.8; delta1=2.5; delta2=0.6                           #PxDelta levels default
+
+
+'''
+Setting delta levels of 2 and 3 is pretty quiet even for volatile stock like GME. Only alerts when thing are really moving
+
+'''
 price = 0
 
 #QA Mode
-qa_prices = [1,1,2,2,4,4,7,7,13,13,7,7,4,4,2,2,1,1]
+qa_prices = [1,1.5,2,2,4,4,7,7.5,13,13,7,7,4,4.5,2,2,1,1]
 if len(sys.argv) > 4: 
     if (sys.argv[4]) == 'qa':
         print ('>4 args')
@@ -78,27 +94,32 @@ def price_alert():
     prices_len = len(qa_prices)
     count = 0
     
-    print ('\n\n\n\n\n\n\nMonitoring Price changes:','['+symbol+'@',price,']')
-    print (price)
     
     while True:
         
         
         #test_mode = 'qa'
+        #import pdb; pdb.set_trace()
         
         if test_mode == 'qa':
             price = qa_prices[count]
+            print ('\n\n\n\n\n\n\nMonitoring Price changes:','['+symbol+'@',price,']')
+            #print (price)
             #print (price)
             #time.sleep(2)
+            if count < prices_len:
+                count += 1
+            else:
+                count = 0
+                break
         
         
         else:
             get_price(symbol)
+            print ('\n\n\n\n\n\n\nMonitoring Price changes------------:','['+symbol+'@',price,']')
+            #print (price)
 
-        if count < prices_len:
-            count += 1
-        else:
-            break
+        
         
         #CRITICAL TRIGGER
         if price > criticalHigh or price < criticalLow:
@@ -109,12 +130,13 @@ def price_alert():
             print ('**************   ',symbol,' PRICE ',price,' !!          ***************\n\nLOG INTO MERRILL NOW! *****\n\n\n\n\n')
         
         
-        #Check last price set
+        #FIRST ITERATION CHECK: Check if last price set
         
         if lastPx == "NULL":        #If lastPx not set yet, set it.(1st run)
             lastPx = price; continue
         
         elif lastPx == price: #If no price change, then loop again
+                print ('UNCH'); time.sleep(2)
                 continue
 
         
@@ -126,12 +148,16 @@ def price_alert():
             PxDelta = round((price-lastPx),2)
             os.system('color 02') # sets the foreground green
             
-            if PxDelta > 2:         #Checking if uptick is large
+            if PxDelta > delta1:         #Checking if uptick is large
                 os.system('color af') # sets the background to light green
                 message = (symbol,price,'up',PxDelta)
                 say(message)
                 playsound('Ring06.wav')
                 print ('^\n^\n^\nuptick - (',PxDelta,') ',price)
+                
+            if PxDelta > delta2:         #Checking if uptick is large
+                message = (symbol,price,'High Volatility, up',PxDelta)
+                say(message)
             
         #            DOWNTICK ALERT ---------   V
             
@@ -141,21 +167,27 @@ def price_alert():
             uptick = "no"
             os.system('color 04') # sets the foreground red
             
-            if PxDelta > 2:         #Checking if downtick is large
+            if PxDelta > delta1:         #Checking if downtick is large
                 os.system('color cf') # sets the background to light red
                 message = (symbol,price,'down',PxDelta)
                 say(message)
                 playsound('down.wav')
                 print ('^\n^\n^\downtick - (',PxDelta,') ',price)
                              
-        if PxDelta > 3:
-            os.system('color 4f') # sets the background to red
-            message = (symbol,price,'High Volatility, down',PxDelta)
-            say(message)
-            playsound('AlarmClock.mp3')
-            print ("PRICE JUMP: ",PxDelta," [Last: ",lastPx," | Price: ",price,']\n')
-            
-        #Medium level alarm for uptick/downtick should be here, then use uptick variable to indicate up/down. Consider change variable to tick=up/down
+            if PxDelta > delta2:
+                os.system('color 4f') # sets the background to red
+                message = (symbol,price,'High Volatility, down',PxDelta)
+                say(message)
+                playsound('AlarmClock.mp3')
+                print ("PRICE DROP: ",PxDelta," [Last: ",lastPx," | Price: ",price,']\n')
+        '''    
+        Medium level alarm for uptick/downtick should be here, then use uptick variable to indicate up/down. Consider change variable to tick=up/down
+        
+        Avg Px pattern display: Maybe on larger price moves, display last 5 average prices:
+            Create average price list and price list, then sum(list)/len(list) > average price list
+            display last 5 values in average price list
+        
+        '''
         
         lastPx = price              #Set last price to current price before starting again
         time.sleep(13)
@@ -175,8 +207,8 @@ if len(sys.argv) < 2:               #DEFAULT PARAMS
 #Load command line paramerters into variables
 else:
     symbol = sys.argv[1]
-    criticalLow = int(sys.argv[2])
-    criticalHigh = int(sys.argv[3])
+    criticalLow = float(sys.argv[2])
+    criticalHigh = float(sys.argv[3])
 
 #Begin price alert loop
 price_alert()
